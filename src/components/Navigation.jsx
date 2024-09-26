cd26f04dd217fdf9532a2739e503073ed03b0f58
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-09-26 14:03:53 +0000

    fine tuning html

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -11,8 +11,9 @@ const Navigation = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           <div className="flex-shrink-0 flex items-center">
-            <span className="font-bold text-xl text-cyber-text cyber-glitch">OCTO & SHARK MUSIC</span>
+            <a href="https://oct-n-shrk-music.netlify.app/" className="font-bold text-xl text-cyber-text cyber-glitch">OCTO & SHARK MUSIC</a>
           </div>
+          <AudioPlayer />
           <div className="hidden md:flex md:items-center md:justify-end md:flex-1">
             {navItems.map((item) => (
               <Button key={item.to} asChild variant="ghost" className="text-cyber-text hover:text-cyber-accent hover:bg-cyber-secondary">
@@ -23,7 +24,6 @@ const Navigation = () => {
               </Button>
             ))}
           </div>
-          <AudioPlayer />
           <div className="flex items-center md:hidden">
             <MobileMenu />
           </div>
--- src/nav-items.jsx
+++ src/nav-items.jsx
@@ -3,12 +3,12 @@ import Index from "./pages/Index.jsx";
 import About from "./pages/About.jsx";
 
 export const navItems = [
-  {
+  /* {
     title: "Home",
     to: "/",
     icon: <HomeIcon className="h-4 w-4" />,
     page: <Index />,
-  },
+  }, */
   {
     title: "About",
     to: "/about",
