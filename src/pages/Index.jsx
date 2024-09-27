57033b17341f7530374ae9c343fc4d8f71b2034e
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-09-27 01:26:39 +0000

    ifrm

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -17,8 +17,8 @@ const Index = () => {
               <CardTitle className="text-cyber-accent">Our Digital Mission</CardTitle>
             </CardHeader>
             <CardContent>
-              <p className="mb-4 text-cyber-text rounded">
-                <iframe class="rounded" id="µ3D_CUBE" src="https://octosharkmusic.github.io/" width="100%" height="333px" title="Floating Cube" scrolling="no" frameborder="0" sandbox="allow-same-origin allow-forms allow-scripts"></iframe>
+              <p className="mb-4 text-cyber-text rounded flex justify-center">
+                <iframe class="rounded" id="µ3D_CUBE_oct-n-shrk" src="https://octosharkmusic.github.io/" width="52%" title="Floating Cube - O&S" scrolling="no" frameborder="0" sandbox="allow-same-origin allow-forms allow-scripts"></iframe>
               </p>
               <Button className="bg-cyber-accent hover:bg-cyber-text hover:text-cyber-background text-cyber-background" asChild>
                 <Link to="/performing">Performing</Link>
