4c2074f02a1e103d37c5cd5c260ca82e9b8acdcf
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-09-27 01:22:06 +0000

    phts

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -5,7 +5,7 @@ import ShaderCanvas from './ShaderCanvas';
 const PhotoGallery = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      {[...Array(6)].map((_, index) => (
+      {[...Array(4)].map((_, index) => (
         <Card key={index} className="overflow-hidden transform transition duration-500 hover:scale-105">
           <CardContent className="p-0 h-48">
             <ShaderCanvas />
