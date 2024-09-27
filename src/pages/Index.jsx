18d760b0321911b63f1b01acfebaaa355cfb6fa7
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-09-27 03:59:40 +0000

    remove h2 s from home page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -25,9 +25,9 @@ const Index = () => {
               </Button>
             </CardContent>
           </Card>
-          <h2 className="text-2xl font-semibold mb-4 text-cyber-accent">Highlight</h2>
+          {/* <h2 className="text-2xl font-semibold mb-4 text-cyber-accent">Highlight</h2> */}
           <ShaderCanvas />
-          <h2 className="text-2xl font-semibold my-8 text-cyber-accent">Media</h2>
+          {/* <h2 className="text-2xl font-semibold my-8 text-cyber-accent">Media</h2> */}
           <PhotoGallery />
         </div>
       </div>
