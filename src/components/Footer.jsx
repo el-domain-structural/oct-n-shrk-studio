82a388ffe9b0c7306eba3f46dd6a9a8bd58ae96c
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-09-27 01:58:17 +0000

    css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ import ShaderCanvas from './ShaderCanvas';
 
 const Footer = () => {
   return (
-    <footer className="bg-cyber-background py-14 mt-12 relative">
+    <footer className="bg-cyber-background py-6 mt-12 relative">
       <div className="absolute inset-0 z-0">
         <ShaderCanvas />
       </div>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center relative z-10">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center relative z-10 my-5 py-3">
         <Button asChild className="bg-cyber-accent hover:bg-cyber-text hover:text-cyber-background text-cyber-background">
           <Link to="/bio">BIOGRAPHY</Link>
         </Button>
