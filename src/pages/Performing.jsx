4febef29bd81d4b7c2d7ff8dc3805d6e8c192cb5
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-09-27 01:15:27 +0000

    swiveal

--- src/pages/Performing.jsx
+++ src/pages/Performing.jsx
@@ -9,10 +9,7 @@ const Performing = () => {
       <div className="explore-future-content">
         <h1 className="explore-future-title cyber-glitch">Performing</h1>
         <div className="explore-future-shader">
-          {/* <ShaderCanvas /> */}
-
           <iframe id="µ3D_CUBE" src="https://octosharkmusic.github.io/events/" width="100%" height="333px" title="Floating Cube" scrolling="no" frameborder="0" sandbox="allow-same-origin allow-forms allow-scripts"></iframe>
-              
         </div>
         <p className="explore-future-text">
           Welcome to the cutting edge of cybernetic innovation. Here, we push the boundaries of human-machine interaction,
@@ -21,7 +18,11 @@ const Performing = () => {
         <div className="explore-future-features">
           <div className="feature">
             <h2>AI Integration</h2>
-            <p>Discover how AI is reshaping our world and enhancing human capabilities.</p>
+            <p>
+              <div className="explore-future-shader">
+                <ShaderCanvas />
+              </div>
+            </p>
           </div>
           <div className="feature">
             <h2>Neural Interfaces</h2>
