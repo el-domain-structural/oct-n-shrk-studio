053c794cf386345a8b3002b2c8cbc6642ae7052f
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-09-27 03:03:54 +0000

    scale ifrm

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -12,7 +12,7 @@ const Index = () => {
       <div className="py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto">
           {/* <h1 className="text-4xl font-bold text-center mb-12 text-cyber-accent cyber-glitch scale-[0.63]">OCTO&SHARK STUDIO</h1> */}
-          <Card className="px-14 mb-8 bg-cyber-secondary bg-opacity-20 backdrop-filter backdrop-blur-lg border-[3px] border-cyber-accent hover:border-2 hover:border-cyber-secondary transition duration-75 hover:duration-300 scale-[0.83]">
+          <Card className="px-14 mb-8 bg-cyber-secondary bg-opacity-20 backdrop-filter backdrop-blur-lg border-[3px] border-cyber-accent hover:border-2 hover:border-cyber-secondary transition duration-75 hover:duration-300 scale-[0.71]">
             {/* <CardHeader>
               <CardTitle className="text-cyber-accent">When & Where can you catch the band</CardTitle>
             </CardHeader> */}
