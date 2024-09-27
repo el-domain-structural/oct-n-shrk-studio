6549ee3a59b226a3a77065b85d2afd9376ccb58c
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-09-27 03:30:57 +0000

    fine tune

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -16,9 +16,9 @@ const Index = () => {
             {/* <CardHeader>
               <CardTitle className="text-cyber-accent">When & Where can you catch the band</CardTitle>
             </CardHeader> */}
-            <CardContent className="justify-center items-center flex flex-col p-1">
+            <CardContent className="justify-center items-center flex flex-col p-1 m-3">
               {/* <p className="mb-4 text-cyber-text rounded-xl flex justify-center"> */}
-              <iframe className="rounded-xl backdrop-opacity-60 backdrop-invert hover:bg-white/30 bg-blue/50 mix-blend-luminosity z-0" id="µ3D_CUBE_oct-n-shrk" src="https://octosharkmusic.github.io/" width="94%" height="512px" title="Floating Cube - O&S" scrolling="no" frameborder="0" sandbox="allow-same-origin allow-forms allow-scripts"></iframe>
+              <iframe className="p-1 rounded-xl backdrop-opacity-60 backdrop-invert hover:bg-white/30 bg-blue/50 mix-blend-luminosity z-0" id="µ3D_CUBE_oct-n-shrk" src="https://octosharkmusic.github.io/" width="94%" height="512px" title="Floating Cube - O&S" scrolling="no" frameborder="0" sandbox="allow-same-origin allow-forms allow-scripts"></iframe>
               {/* </p> */}
               <Button className="bg-cyber-accent hover:bg-cyber-text/30 hover:text-cyber-background/90 hover:outline-2 hover:outline hover:outline-cyber-accent/50 text-cyber-background w-1/3" asChild>
                 <Link to="/performing">Performing</Link>
