58bd010c1f44c134a89ab566aae8ba0395d8f177
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-09-27 03:41:36 +0000

    remove hm

--- src/nav-items.jsx
+++ src/nav-items.jsx
@@ -3,12 +3,12 @@ import Index from "./pages/Index.jsx";
 import Bio from "./pages/Bio.jsx";
 
 export const navItems = [
-  {
+  /* {
     title: "Home",
     to: "/",
     icon: <HomeIcon className="h-4 w-4" />,
     page: <Index />,
-  },
+  }, */
   {
     title: "Bio",
     to: "/bio",
