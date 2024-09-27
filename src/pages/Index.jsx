02a8d6817fa351a8e1bc6f6545c3ef36f18a2b29
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-09-27 03:07:37 +0000

    fine tune

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -12,13 +12,13 @@ const Index = () => {
       <div className="py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto">
           {/* <h1 className="text-4xl font-bold text-center mb-12 text-cyber-accent cyber-glitch scale-[0.63]">OCTO&SHARK STUDIO</h1> */}
-          <Card className="px-14 mb-8 bg-cyber-secondary bg-opacity-20 backdrop-filter backdrop-blur-lg border-[3px] border-cyber-accent hover:border-2 hover:border-cyber-secondary transition duration-75 hover:duration-300 scale-[0.67]">
+          <Card className="px-14 mb-8 bg-cyber-secondary bg-opacity-20 backdrop-filter backdrop-blur-lg border-[3px] border-cyber-accent hover:border-2 hover:border-cyber-secondary transition duration-75 hover:duration-300 scale-[0.6]">
             {/* <CardHeader>
               <CardTitle className="text-cyber-accent">When & Where can you catch the band</CardTitle>
             </CardHeader> */}
             <CardContent className="justify-center flex flex-col p-4 m-0.5">
               <p className="mb-4 text-cyber-text rounded-xl flex justify-center">
-                <iframe className="rounded-xl mt-6 mb-3 p-1 backdrop-opacity-60 backdrop-invert bg-white/30 mix-blend-luminosity z-0" id="µ3D_CUBE_oct-n-shrk" src="https://octosharkmusic.github.io/" width="94%" height="420px" title="Floating Cube - O&S" scrolling="no" frameborder="0" sandbox="allow-same-origin allow-forms allow-scripts"></iframe>
+                <iframe className="rounded-xl mt-6 mb-3 p-1 backdrop-opacity-60 backdrop-invert bg-white/30 mix-blend-luminosity z-0" id="µ3D_CUBE_oct-n-shrk" src="https://octosharkmusic.github.io/" width="94%" height="512px" title="Floating Cube - O&S" scrolling="no" frameborder="0" sandbox="allow-same-origin allow-forms allow-scripts"></iframe>
               </p>
               <Button className="bg-cyber-accent hover:bg-cyber-text hover:text-cyber-background text-cyber-background scale-150 m-3 p-4" asChild>
                 <Link to="/performing">Performing</Link>
