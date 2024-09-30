adcc1aba823347598a86968fe6855d7a2d5d6b36
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-09-30 06:57:24 +0000

    css nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,7 +7,7 @@ import AudioPlayer from './AudioPlayer';
 
 const Navigation = () => {
   return (
-    <nav className="bg-cyber-background border-b border-cyber-accent">
+    <nav className="bg-cyber-background border-b border-cyber-accent z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           <div className="flex-shrink-0 flex items-center">
