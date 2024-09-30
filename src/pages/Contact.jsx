9c3116a5322c094c0da3ec16e7e226ba6cddad50
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-09-30 06:40:22 +0000

    Contact Pge

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,11 +13,11 @@ const Contact = () => {
                 <h1 className="text-4xl font-bold text-center mb-12 text-cyber-accent cyber-glitch">CONTACT</h1>
               </CardTitle>
             </CardHeader>
-            <CardContent>
-              <p className="mb-4 text-cyber-text"><a href='mailto:alx@oct-n-shrk.studio'>alx@oct-n-shrk.studio</a></p>
-              <p className="mb-4 text-cyber-text"><a href='mailto:b3l4@oct-n-shrk.studio'>b3l4@oct-n-shrk.studio</a></p>
-              <p className="text-cyber-text"><a href='mailto:booking@oct-n-shrk.studio'>booking@oct-n-shrk.studio</a></p>
-              <p className="text-cyber-text">
+            <CardContent className="flex flex-col justify-center items-center">
+              <p className="mb-4 text-cyber-text flex flex-col justify-center items-center"><a href='mailto:alx@oct-n-shrk.studio'>alx@oct-n-shrk.studio</a></p>
+              <p className="mb-4 text-cyber-text flex flex-col justify-center items-center"><a href='mailto:b3l4@oct-n-shrk.studio'>b3l4@oct-n-shrk.studio</a></p>
+              <p className="text-cyber-text flex flex-col justify-center items-center"><a href='mailto:booking@oct-n-shrk.studio'>booking@oct-n-shrk.studio</a></p>
+              <p className="text-cyber-text flex flex-col justify-center items-center">
                 <a href='https://linktr.ee/Octo_Shark'>
                     {<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 25" height="16px" style="display: block; width: auto;">
                         <path d="M13.5108 5.85343L17.5158 1.73642L19.8404 4.11701L15.6393 
