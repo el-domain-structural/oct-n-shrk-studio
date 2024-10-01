7ceb6b9ac7714629a29adc80303433d81e514c9f
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-10-01 03:46:41 +0000

    Re - Design - UX / UI ( HOMEPAGE )

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -17,25 +17,25 @@ const Index = () => {
         <div className="fullscreen-video-content"> */}
           <div className="py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-7xl mx-auto">
-              {/* <h1 className="text-4xl font-bold text-center mb-12 text-cyber-accent cyber-glitch scale-[0.63]">OCTO&SHARK STUDIO</h1> */}
+              <h1 className="text-4xl font-bold text-center mb-12 text-cyber-accent cyber-glitch scale-[0.63]">WELCOME</h1>
               <Card className="p-4 mb-2 md:mb-4 bg-cyber-secondary bg-opacity-20 backdrop-filter backdrop-blur-lg border-[3px] border-cyber-accent hover:border-2 hover:border-cyber-secondary transition duration-75 hover:duration-300 scale-[0.86]">
                 {/* <CardHeader>
-                  <CardTitle className="text-cyber-accent">When & Where can you catch the band</CardTitle>
+                  <CardTitle className="text-cyber-accent">Three.Js Cube</CardTitle>
                 </CardHeader> */}
                 <CardContent className="justify-center items-center flex flex-col p-1 m-3">
-                  {/* <p className="mb-4 text-cyber-text rounded-xl flex justify-center"> */}
-                  <iframe className="m-6 p-1 rounded-xl backdrop-opacity-60 backdrop-invert hover:bg-white/30 bg-blue/50 mix-blend-luminosity z-0" id="µ3D_CUBE_oct-n-shrk" src="https://octosharkmusic.github.io/" width="94%" height="512px" title="Floating Cube - O&S" scrolling="no" frameborder="0" sandbox="allow-same-origin allow-forms allow-scripts"></iframe>
-                  {/* </p> */}
+                  <p className="mb-4 text-cyber-text rounded-xl flex justify-center">
+                    <iframe className="m-6 p-1 rounded-xl backdrop-opacity-60 backdrop-invert hover:bg-white/30 bg-blue/50 mix-blend-luminosity z-0" id="µ3D_CUBE_oct-n-shrk" src="https://octosharkmusic.github.io/" width="94%" height="512px" title="Floating Cube - O&S" scrolling="no" frameborder="0" sandbox="allow-same-origin allow-forms allow-scripts"></iframe>
+                  </p>
                   <Button className="mt-2 bg-cyber-accent hover:bg-cyber-background hover:text-cyber-background hover:outline-4 hover:outline hover:outline-white text-cyber-background w-1/3" asChild>
-                    <Link to="/performing">Performing</Link>
+                    <Link to="/performing">AGENDA OF GIGS</Link>
                   </Button>
                 </CardContent>
               </Card>
               <div class="w-full my-4 py-4 scale-95">
-              {/* <h2 className="text-2xl font-semibold mb-4 text-cyber-accent">Highlight</h2> */}
-                <ShaderCanvas />
+                <h2 className="text-2xl font-semibold mb-4 text-cyber-accent">MUSIC</h2>
+                <iframe className="rounded-xl backdrop-opacity-60 backdrop-invert hover:bg-white/30 bg-blue/50 mix-blend-luminosity z-20" id="plgrnd23_oct-n-shrk" src="/* https://oct-n-shrk.media/ */https://belaprodev.github.io/playground/" width="94%" height="512px" title="Floating Cube - O&S" scrolling="no" frameborder="0" sandbox="allow-same-origin allow-forms allow-scripts"></iframe>
               </div>
-              {/* <h2 className="text-2xl font-semibold my-8 text-cyber-accent">Media</h2> */}
+              <h2 className="text-2xl font-semibold my-8 text-cyber-accent">VISUALS</h2>
               <div class="w-full my-4 py-4 scale-95">
                 <PhotoGallery />
               </div>
