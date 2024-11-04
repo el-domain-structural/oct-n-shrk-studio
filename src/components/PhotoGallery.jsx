ab67540c14a56f64587f31d35443b815029c62b8
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-11-04 11:05:24 +0000

    m444t

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -5,7 +5,7 @@ import ShaderCanvas from './ShaderCanvas';
 const PhotoGallery = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      <Card key={index} className="overflow-hidden transform transition duration-500 hover:scale-105">
+      <Card key=33 className="overflow-hidden transform transition duration-500 hover:scale-105">
         <CardContent className="p-0 h-48">
           <iframe width="560" height="315" src="https://www.youtube.com/embed/ALYZa90ZfJw" title="m44444t 1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </CardContent>
