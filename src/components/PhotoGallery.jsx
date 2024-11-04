b329cece699ff6140c8d75035301f4d3723bad76
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-11-04 11:20:18 +0000

    m44444t 1

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -6,13 +6,13 @@ const PhotoGallery = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       <Card key={33} className="overflow-hidden transform transition duration-500 hover:scale-105">
-        <CardContent className="p-0 h-48">
+        <CardContent className="p-0 h-96">
           <iframe width="560" height="315" src="https://www.youtube.com/embed/ALYZa90ZfJw" title="m44444t 1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </CardContent>
       </Card>
-      {[...Array(5)].map((_, index) => (
+      {[...Array(3)].map((_, index) => (
         <Card key={index} className="overflow-hidden transform transition duration-500 hover:scale-105">
-          <CardContent className="p-0 h-48">
+          <CardContent className="p-0 h-96">
             <ShaderCanvas />
           </CardContent>
         </Card>
