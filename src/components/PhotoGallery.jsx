9188547486fb059358da3ad5075e19a373c0fc5e
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-11-04 11:43:51 +0000

    responsive yt

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -6,13 +6,13 @@ const PhotoGallery = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       <Card key={33} className="overflow-hidden transform transition duration-500 hover:scale-105">
-        <CardContent className="p-0 h-64">
+        <CardContent className="p-0 h-60">
           <iframe className="aspect-video w-full" src="https://www.youtube.com/embed/ALYZa90ZfJw" title="m44444t 1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </CardContent>
       </Card>
       {[...Array(3)].map((_, index) => (
         <Card key={index} className="overflow-hidden transform transition duration-500 hover:scale-105">
-          <CardContent className="p-0 h-64">
+          <CardContent className="p-0 h-60">
             <ShaderCanvas />
           </CardContent>
         </Card>
