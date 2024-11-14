3ebbf50d3f067d1c86b213249eb6e2347fa810aa
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-11-14 22:23:25 +0000

    fix

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -7,7 +7,7 @@ const PhotoGallery = () => {
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       <Card key={33} className="overflow-hidden transform transition duration-500 hover:scale-105">
         <CardContent className="p-0 h-60">
-          <iframe className="aspect-video w-full" src="https://www.youtube.com/embed/ALYZa90ZfJw" title="m44444t 1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe className="aspect-video w-full" src="https://www.youtube.com/embed/ALYZa90ZfJw" title="m44444t 1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </CardContent>
       </Card>
       <Card key={22} className="overflow-hidden transform transition duration-500 hover:scale-105">
