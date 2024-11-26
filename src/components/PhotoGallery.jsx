1d6336d02d7430db4284d2a385e74913e531a404
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-11-26 19:19:11 +0000

    Spilled Coffee

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -12,7 +12,7 @@ const PhotoGallery = () => {
       </Card>
       <Card key={22} className="overflow-hidden transform transition duration-500 hover:scale-105">
         <CardContent className="p-0 h-60">
-          <ShaderCanvas />
+          <iframe className="aspect-video w-full" src="https://www.youtube.com/embed/9rpXvwfJMTk" title="m44444t 1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </CardContent>
       </Card>
       <Card key={55} className="overflow-hidden transform transition duration-500 hover:scale-105">
