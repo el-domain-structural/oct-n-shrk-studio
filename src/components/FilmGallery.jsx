11f0fde484c5ff97e07443ae4c8400ce7eaac0e0
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-11-27 16:41:48 +0000

    tour de samme live stream

--- src/components/FilmGallery.jsx
+++ src/components/FilmGallery.jsx
@@ -7,22 +7,22 @@ const FilmGallery = () => {
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       <Card key={33} className="overflow-hidden transform transition duration-500 hover:scale-105">
         <CardContent className="p-0 h-60">
-          <iframe className="aspect-video w-full" src="https://www.youtube.com/embed/Q9uC3SSE-yY" title="m44444t 1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe className="aspect-video w-full" src="https://www.youtube.com/embed/Q9uC3SSE-yY" title="m44444t 1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"  allowfullscreen="true"></iframe>
         </CardContent>
       </Card>
       <Card key={22} className="overflow-hidden transform transition duration-500 hover:scale-105">
         <CardContent className="p-0 h-60">
-          <iframe className="aspect-video w-full" src="https://www.youtube.com/embed/PNKzHGJgkTA" title="m44444t 1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe className="aspect-video w-full" src="https://www.youtube.com/embed/PNKzHGJgkTA" title="m44444t 1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"  allowfullscreen="true"></iframe>
         </CardContent>
       </Card>
       <Card key={55} className="overflow-hidden transform transition duration-500 hover:scale-105">
         <CardContent className="p-0 h-60">
-          <iframe className="aspect-video w-full" src="https://www.youtube.com/embed/D8ZnxvXyxHk" title="m44444t 1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe className="aspect-video w-full" src="https://www.youtube.com/embed/D8ZnxvXyxHk" title="m44444t 1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"  allowfullscreen="true"></iframe>
         </CardContent>
       </Card>
       <Card key={55} className="overflow-hidden transform transition duration-500 hover:scale-105">
         <CardContent className="p-0 h-60">
-          <iframe className="aspect-video w-full" src="https://www.youtube.com/embed/vuTyZ2sPUnk" title="m44444t 1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe className="aspect-video w-full" src="https://www.youtube.com/embed/vuTyZ2sPUnk" title="m44444t 1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"  allowfullscreen="true"></iframe>
         </CardContent>
       </Card>
       <Card key={55} className="overflow-hidden transform transition duration-500 hover:scale-105">
