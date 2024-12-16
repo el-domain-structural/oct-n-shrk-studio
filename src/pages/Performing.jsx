d5a98d25d44ebc5f9c1bd02a9cb36b7b6eeb7eba
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-12-16 13:33:01 +0000

    IG

--- src/pages/Performing.jsx
+++ src/pages/Performing.jsx
@@ -10,6 +10,7 @@ const Performing = () => {
       <div className="explore-future-content">
         <h1 className="explore-future-title cyber-glitch">Up Soon :</h1>
         <div className="explore-future-shader">
+          <InstagramEmbed url="https://www.instagram.com/p/CGIOi2fhWSN/" width={860} />
           <ShaderCanvas />
         </div>
         <div style={{ display: 'flex', justifyContent: 'center' }}>
